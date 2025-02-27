128563459111e7db4ae3fc7343d87912c0b99e82
Caicai <yohna415@gmail.com>
2025-02-27 13:28:52 +0800

    2/27 deploy

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,113 @@
 <template>
-  <v-banner class="banner" height="1100px"> 首頁 INDEX</v-banner>
+  <!-- <v-banner class="banner" height="1100px"> 首頁 INDEX</v-banner> -->
+  <!-- 調色盤網格 -->
+  <v-container class="glass-card mt-5" fluid>
+    <v-row class="flex-child justify-center">
+      <v-col cols="12" md="4" class="d-flex">
+        <v-row class="ma-n3">
+          <!-- 第一欄 照片-->
+          <v-col v-for="(item, index) in firstColItems" :key="index" cols="3">
+            <v-sheet
+              class="d-flex align-center justify-center"
+              :color="item.type === 'color' ? item.value : ''"
+              height="150"
+            >
+              <template v-if="item.type === 'image'">
+                <v-img :src="item.value" max-width="100%" cover></v-img>
+              </template>
+              <template v-else>
+                <!-- SVG 讓文字呈現水彩暈染 -->
+                <svg width="100%" height="100%">
+                  <defs>
+                    <filter id="watercolor">
+                      <feTurbulence
+                        type="fractalNoise"
+                        baseFrequency="0.02"
+                        numOctaves="3"
+                        result="noise"
+                      >
+                        <animate
+                          attributeName="baseFrequency"
+                          values="0.015; 0.02; 0.025; 0.015"
+                          dur="5s"
+                          repeatCount="indefinite"
+                        />
+                      </feTurbulence>
+                      <feDisplacementMap in="SourceGraphic" in2="noise" scale="15" />
+                      <feGaussianBlur stdDeviation="2" />
+                    </filter>
+                  </defs>
+
+                  <!-- 水彩風格文字 -->
+                  <text
+                    x="50%"
+                    y="50%"
+                    text-anchor="middle"
+                    dominant-baseline="middle"
+                    font-size="24"
+                    font-weight="bold"
+                    fill="rgba(0, 0, 0, 0.6)"
+                    filter="url(#watercolor)"
+                  >
+                    先抖一下
+                  </text>
+                </svg>
+              </template>
+            </v-sheet>
+          </v-col>
+
+          <v-col v-for="(item, index) in thirdColItems" :key="index" cols="12">
+            <v-sheet
+              class="d-flex align-center justify-center"
+              :color="item.type === 'color' ? item.value : ''"
+              height="600"
+            >
+              <template v-if="item.type === 'image'">
+                <v-img :src="item.value" max-width="100%" cover></v-img>
+              </template>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <!-- 第二欄  -->
+      <v-col cols="12" md="4" class="d-flex">
+        <v-row class="ma-n3">
+          <v-col v-for="(item, index) in secondColItems" :key="index" cols="3">
+            <v-sheet
+              class="d-flex align-center justify-center"
+              :color="item.type === 'color' ? item.value : ''"
+              height="150"
+            >
+              <template v-if="item.type === 'image'">
+                <v-img :src="item.value" max-width="100%" cover></v-img>
+              </template>
+            </v-sheet>
+          </v-col>
+
+          <v-col v-for="(item, index) in fourthColItems" :key="index" cols="12">
+            <v-sheet
+              class="d-flex align-center justify-center"
+              :color="item.type === 'color' ? item.value : ''"
+              height="600"
+            >
+              <template v-if="item.type === 'image'">
+                <v-img :src="item.value" max-width="100%" cover></v-img>
+              </template>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="12" md="4" class="d-flex ma-n3">
+        <v-col cols="12">
+          <v-sheet class="d-flex" color="bg-blue" height="774">
+            <sheet-footer> #1: (3r x 2c) </sheet-footer>
+          </v-sheet>
+        </v-col>
+      </v-col>
+    </v-row>
+  </v-container>
 
   <!-- 輪播圖 -->
   <v-container class="my-5 d-flex justify-center">
@@ -33,26 +141,34 @@
   <h1 class="text-center">活動月曆</h1>
   <EventCalendar @update-events="(events) => (eventsData = events)" />
 
-  <v-container class="bg-blue" fluid>
-    <h1 class="text-center">最新活動消息</h1>
+  <!-- 最新消息 -->
+  <v-container class="bg-blue py-10" fluid>
+    <h1 class="text-center pt-10">最新活動消息</h1>
 
     <!-- timeline要做RWD 不然會出事 -->
-    <v-card style="max-width: 1200px" class="mx-auto">
-      <v-timeline align="start">
+    <v-card style="max-width: 1200px" class="mx-auto bg-transparent" elevation="0">
+      <v-timeline align="start" density="dense">
         <v-timeline-item v-for="event in latestEvents" :key="event._id" size="large">
           <template #icon>
-            <v-tooltip bottom>
-              <template #activator="{ on, attrs }">
-                <v-avatar v-bind="attrs" :image="event.image" v-on="on"></v-avatar>
-              </template>
-              <v-img :src="event.image" max-width="200"></v-img>
-            </v-tooltip>
+            <v-hover v-slot="{ isHovering, props }">
+              <v-avatar v-bind="props">
+                <v-img :src="event.image" cover></v-img>
+              </v-avatar>
+              <!-- 顯示大圖 -->
+              <v-img
+                v-if="isHovering"
+                :src="event.image"
+                max-width="500px"
+                class="pa-2 mt-2"
+              ></v-img>
+            </v-hover>
           </template>
+
           <template #opposite>
             <span>{{ event.createdAt }} update</span>
           </template>
 
-          <v-card class="rounded-xl"
+          <v-card class="rounded-xl glass-card"
             ><v-card-subtitle>{{ event.start }}</v-card-subtitle>
             <v-card-title>{{ event.title }} </v-card-title>
           </v-card>
@@ -60,10 +176,12 @@
       </v-timeline>
     </v-card>
   </v-container>
+
+  <v-footer> 到底要不要做footer呢 </v-footer>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { useAxios } from '@/composables/axios'
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue'
@@ -118,6 +236,42 @@ watch(eventsData, (newEvents) => {
 })
 
 console.log(latestEvents)
+
+// 首頁banner
+const colors = ['#ffa6a6', '#f9a8d4', '#decfff', '#cff9ff', '#b9ffb3', '#fffcaf', '#ffbf95']
+// 隨機生成 items 陣列
+const generateItems = (count, usedImages) => {
+  const images = posts.value.map((post) => post.image)
+  const generatedItems = []
+  for (let i = 0; i < count; i++) {
+    const isImage = Math.random() > 0.2 // 50% 機率
+    if (isImage && images.length > usedImages.size) {
+      let randomImage
+      do {
+        randomImage = images[Math.floor(Math.random() * images.length)]
+      } while (usedImages.has(randomImage))
+      usedImages.add(randomImage)
+      generatedItems.push({
+        type: 'image',
+        value: randomImage,
+      })
+    } else {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)]
+      generatedItems.push({
+        type: 'color',
+        value: randomColor,
+      })
+    }
+  }
+  return generatedItems
+}
+
+const usedImages = new Set()
+
+const firstColItems = computed(() => generateItems(4, usedImages))
+const secondColItems = computed(() => generateItems(4, usedImages))
+const thirdColItems = computed(() => generateItems(1, usedImages))
+const fourthColItems = computed(() => generateItems(1, usedImages))
 </script>
 
 <style>
@@ -138,6 +292,11 @@ console.log(latestEvents)
 <style scoped>
 .banner {
   background: url('@/assets/banner.png') center center;
+}
+
+.water-mark {
+  color: rgba(0, 0, 0, 0.3);
+  mix-blend-mode: multiply;
 }
 </style>
 
--- src/pages/item.vue
+++ src/pages/item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <v-card height="500px"
       ><vue-particles
         id="tsparticles"
@@ -8,429 +8,271 @@
 
       <vue-particles id="tsparticles" :options="dynamicOptions" @particles-loaded="particlesLoaded"
     /></v-card>
-  </div>
+  </div> -->
 
   <!-- https://vuetifyjs.com/en/components/sheets/#color -->
 
-<v-container class="bg-info mt-10" fluid>
- <v-row class="flex-child">
-
-  <v-col cols="12" md="4" class="bg-pink d-flex">
-
-    <v-row class="ma-n3"  >
-    <v-col cols="3">
-            <v-sheet
-              class="d-flex"
-              color="green-lighten-3"
-              height="150"
-            >
-             <v-img
-        src="@/assets/banner.png"
-             max-width="100%"
-            cover
-      ></v-img>
-
-            </v-sheet>
-          </v-col>
-
-          <v-col cols="3">
-            <v-sheet
-              class="d-flex"
-              color="yellow-lighten-3"
-              height="150"
-            >
-              <sheet-footer>
-                #3: (1r x 1c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-
-           <v-col cols="3">
-            <v-sheet
-              class="d-flex"
-              color="yellow-lighten-3"
-              height="150"
-            >
-              <sheet-footer>
-                #3: (1r x 1c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-
-           <v-col cols="3">
-            <v-sheet
-              class="d-flex"
-              color="yellow-lighten-3"
-              height="150"
-            >
-              <sheet-footer>
-                #3: (1r x 1c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-
-            <v-col cols="12">
-            <v-sheet
-              class="d-flex"
-              color="red-lighten-3"
-              height="250"
-            >
-              <sheet-footer>
-                #5: (2r x 2c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-    </v-row>
-  </v-col>
-
-  <v-col cols="12" md="4" class="bg-purple d-flex">
-
-    <v-row class="ma-n3"  >
-    <v-col cols="3">
-            <v-sheet
-              class="d-flex"
-              color="green-lighten-3"
-              height="150"
-            >
-              <sheet-footer>
-                #2: (1r x 1c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-
-          <v-col cols="3">
-            <v-sheet
-              class="d-flex"
-              color="yellow-lighten-3"
-              height="150"
-            >
-              <sheet-footer>
-                #3: (1r x 1c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-
-           <v-col cols="3">
-            <v-sheet
-              class="d-flex"
-              color="yellow-lighten-3"
-              height="150"
-            >
-              <sheet-footer>
-                #3: (1r x 1c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-
-           <v-col cols="3">
-            <v-sheet
-              class="d-flex"
-              color="yellow-lighten-3"
-              height="150"
-            >
-              <sheet-footer>
-                #3: (1r x 1c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-
-            <v-col cols="12">
-            <v-sheet
-              class="d-flex"
-              color="red-lighten-3"
-              height="250"
-            >
-              <sheet-footer>
-                #5: (2r x 2c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-    </v-row>
-  </v-col>
-
-  <v-col cols="12" md="4" class="bg-blue d-flex">
-     <v-col cols="12">
-      <v-sheet
-          class="d-flex"
-          color="grey-lighten-3"
-          height="424"
-        >
-          <sheet-footer>
-            #1: (3r x 2c)
-          </sheet-footer>
-        </v-sheet>
-    </v-col>
-
-  </v-col>
- </v-row>
-</v-container>
-
-<v-container>
-    <v-row class="flex-child text-subtitle-2">
-      <v-col
-        class="d-flex"
-        cols="12"
-        md="4"
-      >
-        <v-sheet
-          class="d-flex"
-          color="grey-lighten-3"
-          height="424"
-        >
-          <sheet-footer>
-            #1: (3r x 2c)
-          </sheet-footer>
-        </v-sheet>
-      </v-col>
-
-      <v-col
-        class="d-flex"
-        cols="12"
-        md="4"
-      >
+  <v-container class="bg-info mt-5" fluid>
+    <v-row class="flex-child justify-center">
+      <v-col cols="12" md="4" class="d-flex">
         <v-row class="ma-n3">
-          <v-col cols="6">
+          <!-- 第一欄 照片-->
+          <v-col v-for="(item, index) in firstColItems" :key="index" cols="3">
             <v-sheet
-              class="d-flex"
-              color="green-lighten-3"
+              class="d-flex align-center justify-center"
+              :color="item.type === 'color' ? item.value : ''"
               height="150"
             >
-              <sheet-footer>
-                #2: (1r x 1c)
-              </sheet-footer>
+              <template v-if="item.type === 'image'">
+                <v-img :src="item.value" max-width="100%" cover></v-img>
+              </template>
             </v-sheet>
           </v-col>
 
-          <v-col cols="6">
+          <v-col v-for="(item, index) in thirdColItems" :key="index" cols="12">
             <v-sheet
-              class="d-flex"
-              color="yellow-lighten-3"
-              height="150"
+              class="d-flex align-center justify-center"
+              :color="item.type === 'color' ? item.value : ''"
+              height="400"
             >
-              <sheet-footer>
-                #3: (1r x 1c)
-              </sheet-footer>
-            </v-sheet>
-          </v-col>
-
-          <v-col cols="12">
-            <v-sheet
-              class="d-flex"
-              color="red-lighten-3"
-              height="250"
-            >
-              <sheet-footer>
-                #5: (2r x 2c)
-              </sheet-footer>
+              <template v-if="item.type === 'image'">
+                <v-img :src="item.value" max-width="100%" cover></v-img>
+              </template>
             </v-sheet>
           </v-col>
         </v-row>
       </v-col>
 
-      <v-col
-        cols="6"
-        md="2"
-      >
-        <v-sheet
-          class="d-flex"
-          color="teal-lighten-3"
-          height="300"
-        >
-          <sheet-footer>
-            #4: (2r x 1c)
-          </sheet-footer>
+      <!-- 第二欄  -->
+      <v-col cols="12" md="4" class="d-flex">
+        <v-row class="ma-n3">
+          <v-col v-for="(item, index) in secondColItems" :key="index" cols="3">
+            <v-sheet
+              class="d-flex align-center justify-center"
+              :color="item.type === 'color' ? item.value : ''"
+              height="150"
+            >
+              <template v-if="item.type === 'image'">
+                <v-img :src="item.value" max-width="100%" cover></v-img>
+              </template>
+            </v-sheet>
+          </v-col>
+
+          <v-col v-for="(item, index) in fourthColItems" :key="index" cols="12">
+            <v-sheet
+              class="d-flex align-center justify-center"
+              :color="item.type === 'color' ? item.value : ''"
+              height="400"
+            >
+              <template v-if="item.type === 'image'">
+                <v-img :src="item.value" max-width="100%" cover></v-img>
+              </template>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="12" md="4" class="d-flex ma-n3">
+        <v-col cols="12">
+          <v-sheet class="d-flex" color="bg-blue" height="574">
+            <sheet-footer> #1: (3r x 2c) </sheet-footer>
+          </v-sheet>
+        </v-col>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container>
+    <v-row class="flex-child text-subtitle-2">
+      <v-col class="d-flex" cols="12" md="4">
+        <v-sheet class="d-flex" color="grey-lighten-3" height="424">
+          <sheet-footer> #1: (3r x 2c) </sheet-footer>
         </v-sheet>
       </v-col>
 
-      <v-col
-        class="d-flex"
-        cols="6"
-        md="2"
-      >
-        <v-sheet
-          class="d-flex mt-auto"
-          color="purple-lighten-3"
-          height="300"
-        >
-          <sheet-footer>
-            #6: (2r x 1c)
-          </sheet-footer>
+      <v-col class="d-flex" cols="12" md="4">
+        <v-row class="ma-n3">
+          <v-col cols="6">
+            <v-sheet class="d-flex" color="green-lighten-3" height="150">
+              <sheet-footer> #2: (1r x 1c) </sheet-footer>
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="6">
+            <v-sheet class="d-flex" color="yellow-lighten-3" height="150">
+              <sheet-footer> #3: (1r x 1c) </sheet-footer>
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12">
+            <v-sheet class="d-flex" color="red-lighten-3" height="250">
+              <sheet-footer> #5: (2r x 2c) </sheet-footer>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="6" md="2">
+        <v-sheet class="d-flex" color="teal-lighten-3" height="300">
+          <sheet-footer> #4: (2r x 1c) </sheet-footer>
+        </v-sheet>
+      </v-col>
+
+      <v-col class="d-flex" cols="6" md="2">
+        <v-sheet class="d-flex mt-auto" color="purple-lighten-3" height="300">
+          <sheet-footer> #6: (2r x 1c) </sheet-footer>
         </v-sheet>
       </v-col>
     </v-row>
   </v-container>
+
+  <div class="watercolor-box">
+    <svg class="watercolor-bg" viewBox="0 0 300 200">
+      <defs>
+        <filter id="watercolor">
+          <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="5" />
+          <feDisplacementMap in="SourceGraphic" scale="20" />
+        </filter>
+      </defs>
+      <rect width="100%" height="100%" fill="lightblue" filter="url(#watercolor)" />
+    </svg>
+    <p class="watermark">Watermark</p>
+  </div>
 </template>
 
-
-
-
-
-<!-- <template>
-  <div id="app">
-    <vue-particles
-      id="tsparticles"
-      :particles-loaded="particlesLoaded"
-      url="http://foo.bar/particles.json"
-    />
-
-    <vue-particles
-      id="tsparticles"
-      :particles-loaded="particlesLoaded"
-      :options="{
-        background: {
-          color: {
-            value: '#0d47a1',
-          },
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: 'push',
-            },
-            onHover: {
-              enable: true,
-              mode: 'repulse',
-            },
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: '#ffffff',
-          },
-          links: {
-            color: '#ffffff',
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outModes: 'bounce',
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: 'circle',
-          },
-          size: {
-            value: { min: 1, max: 5 },
-          },
-        },
-        detectRetina: true,
-      }"
-    />
-  </div>
-</template> -->
-
-
-
 <script setup>
-import { ref } from 'vue'
-// https://particles.js.org/samples/index.html#growing
-// https://github.com/tsparticles/vue3?tab=readme-ov-file
-const particlesLoaded = async (container) => {
-  console.log('Particles container loaded', container)
+import { ref, computed } from 'vue'
+import { useAxios } from '@/composables/axios'
+
+const { api } = useAxios()
+const posts = ref([])
+
+const getPosts = async () => {
+  try {
+    const { data } = await api.get('/post/random')
+    posts.value.push(...data.result)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-const dynamicOptions = ref({
-  fullScreen: { enable: false },
-  background: {
-    color: {
-      value: '#0d47a1',
-    },
-  },
-  fpsLimit: 120,
-  interactivity: {
-    events: {
-      onClick: {
-        enable: true,
-        mode: 'push',
-      },
-      onHover: {
-        enable: true,
-        mode: 'repulse',
-      },
-    },
-    modes: {
-      bubble: {
-        distance: 400,
-        duration: 2,
-        opacity: 0.8,
-        size: 40,
-      },
-      push: {
-        quantity: 4,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-    },
-  },
-  particles: {
-    color: {
-      value: '#ffffff',
-    },
-    links: {
-      color: '#ffffff',
-      distance: 150,
-      enable: true,
-      opacity: 0.5,
-      width: 1,
-    },
-    move: {
-      direction: 'none',
-      enable: true,
-      outModes: 'bounce',
-      random: false,
-      speed: 6,
-      straight: false,
-    },
-    number: {
-      density: {
-        enable: true,
-      },
-      value: 80,
-    },
-    opacity: {
-      value: 0.5,
-    },
-    shape: {
-      type: 'circle',
-    },
-    size: {
-      value: { min: 1, max: 5 },
-    },
-  },
-  detectRetina: true,
-})
+getPosts()
+console.log(posts.value[0])
+
+const colors = ['#ffa6a6', '#f9a8d4', '#decfff', '#cff9ff', '#b9ffb3', '#fffcaf', '#ffbf95']
+
+// 隨機生成 items 陣列
+// const items = computed(() => {
+//   if (posts.value.length > 0) {
+//     const images = posts.value.map((post) => post.image)
+//     const usedColors = new Set()
+//     const usedImages = new Set()
+//     const generatedItems = []
+//     for (let i = 0; i < 4; i++) {
+//       const isImage = Math.random() > 0.5 // 50% 機率
+//       if (isImage) {
+//         let randomImage
+//         do {
+//           randomImage = images[Math.floor(Math.random() * images.length)]
+//         } while (usedImages.has(randomImage))
+//         usedImages.add(randomImage)
+//         generatedItems.push({
+//           type: 'image',
+//           value: randomImage,
+//         })
+//       } else {
+//         let randomColor
+//         do {
+//           randomColor = colors[Math.floor(Math.random() * colors.length)]
+//         } while (usedColors.has(randomColor))
+//         usedColors.add(randomColor)
+//         generatedItems.push({
+//           type: 'color',
+//           value: randomColor,
+//         })
+//       }
+//     }
+//     return generatedItems
+//   } else {
+//     return []
+//   }
+// })
+
+// 隨機生成 items 陣列
+const generateItems = (count, usedImages) => {
+  const images = posts.value.map((post) => post.image)
+  const generatedItems = []
+  for (let i = 0; i < count; i++) {
+    const isImage = Math.random() > 0.2 // 50% 機率
+    if (isImage && images.length > usedImages.size) {
+      let randomImage
+      do {
+        randomImage = images[Math.floor(Math.random() * images.length)]
+      } while (usedImages.has(randomImage))
+      usedImages.add(randomImage)
+      generatedItems.push({
+        type: 'image',
+        value: randomImage,
+      })
+    } else {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)]
+      generatedItems.push({
+        type: 'color',
+        value: randomColor,
+      })
+    }
+  }
+  return generatedItems
+}
+
+const usedImages = new Set()
+
+const firstColItems = computed(() => generateItems(4, usedImages))
+const secondColItems = computed(() => generateItems(4, usedImages))
+const thirdColItems = computed(() => generateItems(1, usedImages))
+const fourthColItems = computed(() => generateItems(1, usedImages))
+
+// const items = computed(() => {
+//   if (posts.value.length > 0) {
+//     return [
+//       {
+//         type: 'image',
+//         value: posts.value[0].image, // 正確存取 posts 的第一個元素
+//         label: '#1: (3r x 2c)',
+//       },
+//       { type: 'color', value: 'green-lighten-3', label: '#2: (1r x 1c)' },
+//       { type: 'color', value: 'yellow-lighten-3', label: '#3: (1r x 1c)' },
+//       { type: 'color', value: 'purple-lighten-3', label: '#4: (1r x 1c)' },
+//     ]
+//   } else {
+//     return []
+//   }
+// })
 </script>
+
+<style scoped>
+.watercolor-box {
+  width: 300px;
+  height: 200px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.watercolor-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.watermark {
+  font-size: 24px;
+  font-weight: bold;
+  color: rgba(0, 0, 0, 0.3);
+  mix-blend-mode: multiply;
+  position: absolute;
+}
+</style>
